cd701a321ae10f5d101484a9754c76af92dc822a
arya-map22 <arya.prayoga222.ap@gmail.com>
2025-01-10 15:27:50 +0700

    Button style revision

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,5 @@
 import { currencyFormatter } from "../utils/currency";
 
-import Button from "./UI/Button";
-
 export default function Product({ title, image, description, price }) {
   return (
     <article className="flex h-80 w-80 flex-col flex-nowrap items-center gap-2 rounded-sm border-2 border-solid border-beige p-2">
@@ -10,9 +8,11 @@ export default function Product({ title, image, description, price }) {
         <img src={image} alt={title} className="h-full w-full" />
       </div>
       <p className="h-20 overflow-scroll p-2 text-justify">{description}</p>
-      <div className="flex w-full px-2 justify-between items-center">
+      <div className="flex w-full items-center justify-between px-2">
         <h3>{currencyFormatter.format(price)}</h3>
-        <Button>Add to Cart</Button>
+        <button className="rounded-md border-2 border-solid border-blue p-1 text-sm hover:bg-blue hover:text-white active:bg-dark-blue">
+          Add to Cart
+        </button>
       </div>
     </article>
   );
