10151fa8a4886c48fcf078879e4e759a6bea8ddc
arya-map22 <arya.prayoga222.ap@gmail.com>
2025-01-10 13:25:53 +0700

    Add a few empty components, configure store, and add MainNavigation

---
+++ src/components/MainNavigation.jsx
@@ -0,0 +1,12 @@
+export default function MainNavigation() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Cart</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
---
+++ src/store/cart.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {},
+
+    removeItem(state, action) {},
+  },
+});
+
+export default cartSlice;
