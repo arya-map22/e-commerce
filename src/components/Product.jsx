c7212cf0fe84bd921ffcf46954c38a5b1ba475f0
arya-map22 <arya.prayoga222.ap@gmail.com>
2025-01-10 16:50:26 +0700

    Add shadow to Product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,7 +2,7 @@ import { currencyFormatter } from "../utils/currency";
 
 export default function Product({ title, image, description, price }) {
   return (
-    <article className="flex h-80 w-80 flex-col flex-nowrap items-center gap-3 rounded-sm border-2 border-solid border-beige p-2">
+    <article className="flex h-80 w-80 flex-col flex-nowrap items-center gap-3 rounded-sm border-2 border-solid border-beige p-2 shadow-md shadow-blue/50 hover:shadow-lg hover:shadow-blue/70">
       <h2>{title}</h2>
       <div className="h-32 w-auto">
         <img src={image} alt={title} className="h-full w-full" />
