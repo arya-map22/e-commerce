073b63631af68a734fb8a5fe571be45f25c82f6a
arya-map22 <arya.prayoga222.ap@gmail.com>
2025-01-10 13:09:01 +0700

    Add colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-blue": "#213555",
+        blue: "#3E5879",
+        beige: "#D8C4B6",
+        "light-beige": "#F5EFE7",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
