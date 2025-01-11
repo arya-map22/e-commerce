9115b50d50bc67d66cb916c79594b08cb29a048c
arya-map22 <arya.prayoga222.ap@gmail.com>
2025-01-11 23:48:23 +0700

    Update MainNavigation
    Add NavLink for Home navigation to HomePage and change styling

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,11 +1,24 @@
+import { NavLink } from "react-router";
+
 export default function MainNavigation() {
   return (
-    <header className="mb-4 bg-dark-blue">
-      <nav>
+    <header className="mb-4 flex w-full items-center bg-dark-blue">
+      <nav className="w-1/2">
         <ul className="mx-4 flex gap-2 p-2 text-white">
-          <li>Home</li>
-          <li>Log In</li>
+          <li>
+            <NavLink
+              className="hover:text-beige active:relative active:top-0.5"
+              to="/"
+            >
+              Home
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <nav className="w-1/2">
+        <ul className="mx-4 flex justify-end gap-2 p-2 text-white">
           <li>Cart</li>
+          <li>Log In</li>
         </ul>
       </nav>
     </header>
