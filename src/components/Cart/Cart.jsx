48c5eeb41c4badd6744f59fb049bebf56cb471ae
arya-map22 <arya.prayoga222.ap@gmail.com>
2025-01-12 18:55:03 +0700

    Fix CartList bug & add Link to CartList
    - Whenever user visit another page, CartList is hide automatically.
    - When user click item in the CartList will open that item page.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useLocation } from "react-router";
 
 import { selectUserCart, selectCartItemsAmount } from "../../store/cart";
 
@@ -8,6 +9,7 @@ import CartList from "./CartList";
 
 export default function Cart() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
+  const location = useLocation();
 
   const itemsAmount = useSelector(selectCartItemsAmount);
   const cart = useSelector(selectUserCart);
@@ -15,6 +17,11 @@ export default function Cart() {
   function handleToggleShowCart() {
     setCartIsVisible((prevState) => !prevState);
   }
+
+  // Hide CartList whenever user visits another page
+  useEffect(() => {
+    setCartIsVisible(false);
+  }, [location.pathname]);
 
   return (
     <div className="relative ml-3">
